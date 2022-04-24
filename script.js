cf34ebe94aa85108222c4a4a565c70fc17190721
habibhamdoun <habibhamdoun@gmail.com>
2022-04-24 06:35:49 +0300

    defined navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+const collapsibles = document.querySelectorAll(".nav__collapsible__container");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    document.getElementById('nav').classList.toggle("collapsible--expanded");
+    
+  })
+); 
